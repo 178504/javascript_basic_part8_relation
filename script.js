d280fce9a5d4f24d2ce3be6797494943eaf46de4
tsujishouta <koshi51455@gmail.com>
2023-06-18 22:08:11 +0900

    second

--- script.js
+++ script.js
@@ -2,24 +2,26 @@
 var x = 504
 var y = 178
 if(x >= y){
-  console.log("xはyより大きいまたは等しい")
+  console.log("xはyより大きいまたは等しい");
 }
+
 //2. xがyより小さい時にifの処理へ入るようにしてください。
 var x = 178
 var y = 504
 if(x <= y){
-  console.log("xがyより小さいまたは等しい")
+  console.log("xがyより小さいまたは等しい");
 }
+
 //3. xがy以上の時にifの処理へ入るようにしてください。
 var x = 504
 var y = 178
 if(x > y){
-  console.log("xがy以上")
+  console.log("xがy以上");
 }
 
 //4. xがy以下の時にifの処理へ入るようにしてください。
 var x = 178
 var y = 504
 if(x < y){
-  console.log("xがy以下")
+  console.log("xがy以下");
 }
