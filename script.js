7b03d82b6a417ce5c815a38ec4d4b7874ddabe5e
tsujishouta <koshi51455@gmail.com>
2023-06-19 22:16:07 +0900

    thred

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //1. xがyより大きい時にifの処理へ入るようにしてください。
 var x = 200
 var y = 100
-if(x >= y){
-  console.log("xはyより大きいまたは等しい");
+if(x > y){
+  console.log("xはyより大きい");
 }
 
 //2. xがyより小さい時にifの処理へ入るようにしてください。
 var x = 100
 var y = 200
-if(x <= y){
-  console.log("xがyより小さいまたは等しい");
+if(x < y){
+  console.log("xがyより小さい");
 }
 
 //3. xがy以上の時にifの処理へ入るようにしてください。
 var x = 200
 var y = 100
-if(x > y){
+if(x >= y){
   console.log("xがy以上");
 }
 
 //4. xがy以下の時にifの処理へ入るようにしてください。
 var x = 100
 var y = 200
-if(x < y){
+if(x <= y){
   console.log("xがy以下");
 }
